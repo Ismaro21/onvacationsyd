d0aa234f81cbdebc07381fb0c54042b5e30c9e14
Ismaro21 <90430290+Ismaro21@users.noreply.github.com>
2024-09-06 18:19:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function openModal() {
-    document.getElementById('pdfModal').style.display = 'flex'; // Mostrar la ventana flotante
-    document.getElementById('pdfViewer').src = 'pdf/black.pdf'; // Ruta al PDF
-}
+function openModal(pdfUrl) {
+            document.getElementById('pdfModal').style.display = 'flex'; // Mostrar la ventana flotante
+            document.getElementById('pdfViewer').src = pdfUrl; // Ruta al PDF
+        }
 
 function closeModal() {
-    document.getElementById('pdfModal').style.display = 'none'; // Ocultar la ventana flotante
-    document.getElementById('pdfViewer').src = ''; // Limpiar la ruta del PDF
-}
+            document.getElementById('pdfModal').style.display = 'none'; // Ocultar la ventana flotante
+            document.getElementById('pdfViewer').src = ''; // Limpiar la ruta del PDF
+        }
