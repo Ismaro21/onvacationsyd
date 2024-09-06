678e550e40eba00ddaca9d12e0fedb048d2f3713
Ismaro21 <90430290+Ismaro21@users.noreply.github.com>
2024-09-06 16:21:19 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function openModal() {
+    document.getElementById('pdfModal').style.display = 'flex'; // Mostrar la ventana flotante
+    document.getElementById('pdfViewer').src = 'catalogo.pdf'; // Ruta al PDF
+}
+
+function closeModal() {
+    document.getElementById('pdfModal').style.display = 'none'; // Ocultar la ventana flotante
+    document.getElementById('pdfViewer').src = ''; // Limpiar la ruta del PDF
+}
